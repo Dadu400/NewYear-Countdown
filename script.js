055330bb74cfbdc1975abb771852f25ef74fcc3a
Dali Khukhunashvili <daduka.xuxunashvili@gmail.com>
2023-08-21 16:22:29 +0400

    newyear

---
+++ script.js
@@ -0,0 +1,26 @@
+const days = document.getElementById("days");
+const hours = document.getElementById("hours");
+const minutes = document.getElementById("minutes");
+const seconds = document.getElementById("seconds");
+
+const currentYear = new Date().getFullYear();
+
+const newYearTime = new Date(`January 1 ${currentYear + 1} 00:00:00`);
+
+function countDowntime() {
+    const currenTime = new Date();
+    const diff = newYearTime - currenTime;
+
+    const d = Math.floor(diff / 1000 / 60 / 60 / 24);
+    const h = Math.floor(diff / 1000 / 60 / 60) % 24;  
+    const m = Math.floor(diff / 1000 / 60) % 60;   
+    const s = Math.floor(diff / 1000) % 60;  
+
+    days.innerHTML = d;
+    hours.innerHTML = h;
+    minutes.innerHTML = m;
+    seconds.innerHTML = s;
+
+}
+
+setInterval(() => countDowntime(), 1000);
